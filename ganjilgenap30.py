cfaf25d4d9b8be175b85ae87ac8bfb5e5cdb585f
30_Reza Putra Pratama <reza.tkjsmeksa@gmail.com>
2026-08-31 08:43:06 +0700

    Add files via upload

--- ganjilgenap30.py
+++ ganjilgenap30.py
@@ -1,15 +1,19 @@
-while True:
-    print("===== PROGRAM CEK BILANGAN =====")
+def cek_bilangan():
+    while True:
+        print("===== PROGRAM CEK BILANGAN =====")
 
-    angka = int(input("Masukkan sebuah bilangan: "))
+        angka = int(input("Masukkan sebuah bilangan: "))
 
-    if angka % 2 == 0:
-        print("Bilangan", angka, "adalah GENAP")
-    else:
-        print("Bilangan", angka, "adalah GANJIL")
+        if angka % 2 == 0:
+            print("Bilangan", angka, "adalah GENAP")
+        else:
+            print("Bilangan", angka, "adalah GANJIL")
 
-    pilihan = input("Apakah ingin mengulang? (y/n): ")
+        pilihan = input("Apakah ingin mengulang? (y/n): ")
 
-    if pilihan.lower() == "n":
-        print("Program selesai.")
-        break
+        if pilihan.lower() == "n":
+            print("Program selesai.")
+            break
+
+
+cek_bilangan()
