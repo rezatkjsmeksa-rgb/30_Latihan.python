f4fe97529a3d123b8c4d0e58283375cbf12ba88a
30_Reza Putra Pratama <reza.tkjsmeksa@gmail.com>
2026-08-31 09:48:34 +0700

    Add files via upload

--- ganjilgenap30.py
+++ ganjilgenap30.py
@@ -16,4 +16,15 @@ def cek_bilangan():
             break
 
 
-cek_bilangan()
+def cek_prima():
+    angka = int(input("Masukkan bilangan: "))
+
+    if angka > 1:
+        for i in range(2, angka):
+            if angka % i == 0:
+                print("Bukan bilangan prima")
+                break
+        else:
+            print("Bilangan prima")
+    else:
+        print("Bukan bilangan prima")
