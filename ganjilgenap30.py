e9208958e6fe36144486496cd6c1fc8f8286b9f7
30_Reza Putra Pratama <reza.tkjsmeksa@gmail.com>
2026-08-31 08:38:19 +0700

    Add files via upload

---
+++ ganjilgenap30.py
@@ -0,0 +1,8 @@
+print("===== PROGRAM CEK BILANGAN =====")
+
+angka = int(input("Masukkan sebuah bilangan: "))
+
+if angka % 2 == 0:
+    print("Bilangan", angka, "adalah GENAP")
+else:
+    print("Bilangan", angka, "adalah GANJIL")
